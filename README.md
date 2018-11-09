dc49f6bf376988f6db04d16f8584bec2ff6b6acf
Guillermo <willstarxey@gmail.com>
2018-11-09 13:49:15 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##Aplicación de comandos
+
+Ejercicio del curso de node
+
+Instalar los paquetes de npm
---
+++ config/yargs.js
@@ -0,0 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente una tarea'
+}
+
+
+const argv = require('yargs')
+    .command('crear', 'Crea una tarea por realizar', { descripcion })
+    .command('actualizar', 'Cambia el estado completado de una tarea', { descripcion, completado })
+    .command('borrar', 'Elimina una tarea', { descripcion })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
